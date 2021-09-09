b39dc64344aab416da7ca2218098e720e3e093d3
Miguel Santiago <miguelsantiago1812@gmail.com>
2021-09-09 15:15:19 -0300

    ✔Inserção de Dados automatica

--- src/main/java/com/IdeaBox/models/usuarios/Administrador.java
+++ src/main/java/com/IdeaBox/models/usuarios/Administrador.java
@@ -3,6 +3,8 @@ package com.IdeaBox.models.usuarios;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+
+import com.IdeaBox.models.cargos.Cargo;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 
 @Entity
@@ -16,4 +18,11 @@ public class Administrador extends Usuario {
 	public Administrador() {
 		
 	}
+
+	public Administrador(long id, String nome, String cpf, Cargo cargo, String login, String senha, String email) {
+		super(id, nome, cpf, cargo, login, senha, email);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT  INTO `ideabox`.`cargo` (`id`, `nome`) VALUES ('1', 'ADMINISTRADOR');
-INSERT  INTO `ideabox`.`administrador` (`id`, `cpf`, `email`, `login`, `nome`, `senha`, `status`, `cargo_id`) VALUES ('1', '89834554647', 'adminsenac@gmail.com', 'admin', 'admin', '123', 'ATIVO', '1');
- 
