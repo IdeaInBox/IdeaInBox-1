85bbfba7d875cd4337eb5bd581781bcd32335356
Davi Saviano Moraes <87396299+Davi1234567891099@users.noreply.github.com>
2021-09-13 15:44:33 -0300

    mudado umas parada la do bugzinho

--- src/main/java/com/IdeaBox/IdeaboxApplication.java
+++ src/main/java/com/IdeaBox/IdeaboxApplication.java
@@ -19,23 +19,25 @@ public class IdeaboxApplication implements CommandLineRunner {
 
 	@Autowired
 	CargoRepository crg;
-	
+
 	@Autowired
 	AdminRepository ar;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(IdeaboxApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo1 = new Cargo(1,"Administrador");
-		Cargo cargo2 = new Cargo(2,"Gerente");
-		
-		Administrador adm = new Administrador(1, "Administrador", "13254743040", cargo1, "admin", "1234567", "admin@senac.com.br");
-		cargo1.getColaborador().add(adm);
-		
-		if(crg.findAll() == null && ar.findAll() == null) {
+
+		if (crg.findAll() == null && ar.findAll() == null) {
+			Cargo cargo1 = new Cargo(1, "Administrador");
+			Cargo cargo2 = new Cargo(2, "Gerente");
+
+			Administrador adm = new Administrador(1, "Administrador", "13254743040", cargo1, "admin", "1234567",
+					"admin@senac.com.br");
+			cargo1.getColaborador().add(adm);
+
 			this.crg.saveAll(Arrays.asList(cargo1, cargo2));
 			this.ar.saveAll(Arrays.asList(adm));
 		}
