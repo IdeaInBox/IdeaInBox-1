221303518db43eafacb38b6a2b0d0e0e9919ef17
Davi Saviano Moraes <87396299+Davi1234567891099@users.noreply.github.com>
2021-09-29 16:39:44 -0300

    corrigido uns bug e alterado o modal do email na home

--- src/main/java/com/IdeaBox/controllers/EmailController.java
+++ src/main/java/com/IdeaBox/controllers/EmailController.java
@@ -66,7 +66,21 @@ public class EmailController {
         }
     }
 
+    @RequestMapping(path = "/email-send1", method = RequestMethod.GET)
+    public String sendMail1(@RequestParam("texto") String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText(text);
+        message.setTo("ideainboxapp@gmail.com");
+        message.setFrom("ideainboxapp@gmail.com");
 
+        try {
+            mailSender.send(message);
+            return "redirect:/";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao enviar email.";
+        }
+    }
 
 //"ideainboxapp@gmail.com"
 }
