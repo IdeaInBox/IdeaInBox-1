ae1603f48c158a0c42301eebc585cfbf16a96b8a
Ligiane Vieira <ligiane_vieira@msn.com>
2021-10-02 20:31:43 -0300

    Acrescentado validações Front -end
    Acrescentado validações nos formulários, algumas validações html com pattern+regex e outras com javascript

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -32,3 +32,33 @@ function limitarCaracter() {
     document.getElementById("botao").disabled = true;
   }
 }
+
+//Formulario
+//cpf
+function validaCpf() {
+  var cpf = document.getElementById("cpf").value;
+
+  var Soma;
+    var Resto;
+    Soma = 0;
+  
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) ){
+    alert ("Cpf inválido") 
+    return;
+}
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) ){
+    alert ("Cpf inválido")
+    return;
+  }  
+}
+
